1feb7bd9fc8f5ce20490ede111ff77e3ff9e01b8
venutti <lvenutti@fi.uba.ar>
2023-02-12 16:48:35 -0300

    agrega detalles de estilos

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,11 +4,11 @@ export default function Hero() {
   return (
     <main className="bg-gray-200 bg-image grow">
       <div className="h-full backdrop-brightness-50 grid place-content-center place-items-center text-center text-white font-mono">
-        <img
+        {/* <img
           className="w-40 h-40 mb-10 rounded-full"
           src={profileImage}
           alt="Imagen de perfil"
-        />
+        /> */}
         <h1 className="text-4xl">LUCAS VENUTTI</h1>
         <h3>Desarrollador frontend de páginas web</h3>
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,5 @@
+import Container from "./Container";
+
 export default function Navbar({ links }) {
   const renderedLinks = links.map((link) => (
     <li
@@ -11,12 +13,12 @@ export default function Navbar({ links }) {
 
   return (
     <header className="bg-cyan-700 text-white p-4">
-      <div className="flex justify-between items-center flex-col gap-2 mx-auto max-w-3xl sm:flex-row">
+      <Container className="flex justify-between items-center flex-col gap-2 sm:flex-row">
         <h2 className="font-semibold text-3xl">Lucas Venutti</h2>
         <nav>
           <ul className="flex gap-4">{renderedLinks}</ul>
         </nav>
-      </div>
+      </Container>
     </header>
   );
 }
