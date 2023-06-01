867dd9be1785dadeba7612949aaa52c3f1e20795
venutti <lvenutti@fi.uba.ar>
2023-06-01 18:55:52 -0300

    add: link a github

---
+++ src/components/GithubIcon/index.jsx
@@ -0,0 +1,10 @@
+import { AiFillGithub } from "react-icons/ai";
+import "./index.css";
+
+export default function GithubIcon() {
+  return (
+    <a href="https://github.com/venutti" target="_blank">
+      <AiFillGithub className="icon" />
+    </a>
+  );
+}
